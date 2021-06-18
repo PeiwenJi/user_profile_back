bd40783ee591efbd43dd98075e7747dd95ed96f3
Administrator <2931473049@qq.com>
2021-06-18 21:42:25 +0800

    用户个人画像后台实现

--- src/main/java/bigdata/com/controller/CustomerController.java
+++ src/main/java/bigdata/com/controller/CustomerController.java
@@ -14,6 +14,7 @@ public class CustomerController {
     @Resource
     private HBaseClient hBaseClient;
 
+    //根据ID获取用户特征
     @RequestMapping("/searchCustomerInfo")
     public String searchCustomerInfo(String id) {
         String userName = hBaseClient.getValue("aft_basic_user", id, "user", "username");
@@ -25,8 +26,8 @@ public class CustomerController {
         String nationality = hBaseClient.getValue("aft_basic_user", id, "user", "nationality");
         String marriage = hBaseClient.getValue("aft_basic_user", id, "user", "marriage");
         String politicalFace = hBaseClient.getValue("aft_basic_user", id, "user", "politicalFace");
-        String source = hBaseClient.getValue("aft_basic_user", id, "user", "source");
-        String qq = hBaseClient.getValue("aft_basic_user", id, "user", "qq");
+        String ageGroup = hBaseClient.getValue("aft_basic_user", id, "user", "ageGroup");
+        String constellation = hBaseClient.getValue("aft_basic_user", id, "user", "constellation");
         String money = hBaseClient.getValue("aft_basic_user", id, "user", "money");
         String registerTime = hBaseClient.getValue("aft_basic_user", id, "user", "registerTime");
         String lastLoginTime = hBaseClient.getValue("aft_basic_user", id, "user", "lastLoginTime");
@@ -41,12 +42,63 @@ public class CustomerController {
         infoList.add(nationality);
         infoList.add(marriage);
         infoList.add(politicalFace);
-        infoList.add(source);
-        infoList.add(qq);
+        infoList.add(ageGroup);
+        infoList.add(constellation);
         infoList.add(money);
         infoList.add(registerTime);
         infoList.add(lastLoginTime);
 
         return JSON.toJSONString(infoList);
+    }
+
+    //根据ID获取消费特征
+    @RequestMapping("/searchConsumptionFeature")
+    public String searchConsumptionFeature(String id){
+        String consumptionCycle = hBaseClient.getValue("aft_basic_biz", id, "biz", "consumptionCycle");
+        String avgOrderAmount = hBaseClient.getValue("aft_basic_biz", id, "biz", "avgOrderAmount");
+        String paymentCode = hBaseClient.getValue("aft_basic_biz", id, "biz", "paymentCode");
+        String maxOrderAmount = hBaseClient.getValue("aft_basic_biz", id, "biz", "maxOrderAmount");
+
+        List<String> consumptionList = new ArrayList<>();
+        consumptionList.add(consumptionCycle);
+        consumptionList.add(avgOrderAmount);
+        consumptionList.add(paymentCode);
+        consumptionList.add(maxOrderAmount);
+
+        return JSON.toJSONString(consumptionList);
+    }
+
+    //根据ID获取行为特征
+    @RequestMapping("/searchBehaviorFeature")
+    public String searchBehaviorFeature(String id){
+        String lastLogin = hBaseClient.getValue("aft_basic_beh", id, "behavior", "lastLogin");
+        String browsePage = hBaseClient.getValue("aft_basic_log", id, "log", "browsePage");
+        String logSession = hBaseClient.getValue("aft_basic_beh", id, "behavior", "logSession");
+        String goodsBought = hBaseClient.getValue("aft_basic_beh", id, "behavior", "goodsBought");
+        String browseFrequency = hBaseClient.getValue("aft_basic_log", id, "log", "browseFrequency");
+        String logFrequency = hBaseClient.getValue("aft_basic_log", id, "log", "logFrequency");
+        String browseTime = hBaseClient.getValue("aft_basic_log", id, "log", "browseTime");
+
+        List<String> behaviorList = new ArrayList<>();
+        behaviorList.add(lastLogin);
+        behaviorList.add(browsePage);
+        behaviorList.add(logSession);
+        behaviorList.add(goodsBought);
+        behaviorList.add(browseFrequency);
+        behaviorList.add(logFrequency);
+        behaviorList.add(browseTime);
+
+        return JSON.toJSONString(behaviorList);
+    }
+
+    //根据ID获取用户价值
+    @RequestMapping("/searchUserValue")
+    public String searchUserValue(String id){
+        String userValue = hBaseClient.getValue("aft_basic_biz", id, "biz", "userValue");
+
+        List<String> valueList = new ArrayList<>();
+        valueList.add(userValue);
+
+        return JSON.toJSONString(valueList);
     }
 }
